c9e9ccca98ddc1be31338db456a13d32a5243420
jamieboy1337 <jamieboy1337@gmail.com>
2020-04-23 02:20:58 -0700

    some note taking

---
+++ app/src/main/java/com/example/studdybuddy/session/SessionProgressReceiver.java
@@ -0,0 +1,19 @@
+package com.example.studdybuddy.session;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class SessionProgressReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context ctx, Intent intent) {
+        /*
+              todo: call this after 30 seconds away from the application
+                    to mark the session as paused
+
+                    probably gonna have to build it from scratch but thats ok :-)
+         */
+    }
+
+}
